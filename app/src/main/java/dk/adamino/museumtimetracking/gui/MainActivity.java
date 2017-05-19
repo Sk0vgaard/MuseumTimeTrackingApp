9a3c8d4835c8e97f3c5357e55379a4bab2ce282e
Adam Hansen <adamlars90@gmail.com>
2017-05-19 13:39:03 +0200

    Updated id

--- app/src/main/java/dk/adamino/museumtimetracking/gui/MainActivity.java
+++ app/src/main/java/dk/adamino/museumtimetracking/gui/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     private void handleDocumentHours() {
         mDocumentHoursButton.setOnClickListener((View v) -> {
 
-            int id = 134; //Adam Hansen
+            int id = 166; //Adam Hansen
             String guild = mGuildSpinner.getSelectedItem().toString();
             Date date = new Date();
             int hours = mNumberPicker.getValue();
